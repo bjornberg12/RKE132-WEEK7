e33c2e74eab63b3a1ed84c087be528eb9054b2ba
Hugo L <hugo.liivaleht@tktk.ee>
2024-10-18 11:43:03 +0300

    fakk git

---
+++ Calculaotr/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+Console.WriteLine("enter the opartion +-");
+char userOperator = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("enter a");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("enter b");
+    int secondNum = Int32.Parse(Console.ReadLine());    
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+
+    case '-':
+        Subtract(firstNum, secondNum); 
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+
+static void Addition(int a, int b)
+{
+
+    Console.WriteLine($"{a} + {b} = {a +b}");
+
+}
+
+static void Subtract(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("are you coming or leaving (in or out)");
+
+
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+} 
+else
+{
+    PrintGoodBye();
+}
+
+
+
+static void PrintHello()
+{
+    Console.WriteLine("hello, world");
+}
+
+static void PrintGoodBye()
+{
+
+    Console.WriteLine("goodbye");
+}
